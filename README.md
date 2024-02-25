97aad49e502fce490c5d550ef150cb503bd38f58
DawarWaqar <dawarwaqar90@gmail.com>
2024-02-25 06:04:14 -0500

    implementation

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # Ensemble-extractive-text-summarization
-Using ensemble of extractive text summarization techniques for BBC News summarization.
+
+Ensemble approach on extractive text summarization techniques for BBC News summarization.
+
+## Quick Start Tutorial
+
+### Clone the repository
+
+git clone https://github.com/ascii-monk123/Ensemble-extractive-text-summarization.git
+
+cd Ensemble-extractive-text-summarization
+
+### Set up virtual environment
+
+python -m venv ensemble_venv
+
+source ensemble_venv/bin/activate
+
+### Install dependencies
+
+pip install -r requirements.txt
+
+### Setup IPython Kernel for Jupyter
+
+python -m ipykernel install --user --name ensemble_kernel
+
+pip install jupyter
+
+jupyter notebook
+
+### Note: Please make sure you have selected the "ensemble_kernel" in Proposed Solution.ipynb
---
+++ main.py
@@ -0,0 +1,133 @@
+import re
+import spacy
+import pandas as pd
+import nltk
+from nltk import sent_tokenize, word_tokenize
+from nltk.tokenize.punkt import PunktSentenceTokenizer
+from textacy import preprocessing as prep
+from spacy.lang.en.stop_words import STOP_WORDS
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.summarizers.lsa import LsaSummarizer
+from rouge import Rouge
+
+
+nltk.download("punkt")
+nltk.download("stopwords")
+
+
+def replace_dots(text):
+    text = re.sub(r"(?<!\d)\.{1,}(?!\d)", ". ", text)
+    return re.sub(r"(?<=\d)\.(?=[^\d]|$)", ". ", text)
+
+
+def preprocess_text(df):
+    # the preprocessing pipeline
+    process = prep.make_pipeline(
+        prep.replace.emojis,
+        prep.replace.emails,
+        prep.replace.urls,
+        prep.replace.phone_numbers,
+        prep.remove.html_tags,
+        prep.normalize.hyphenated_words,
+        prep.normalize.whitespace,
+        prep.normalize.unicode,
+        prep.normalize.quotation_marks,
+        prep.normalize.bullet_points,
+    )
+
+    # apply the pre-processing pipeline on the dataframe
+    df.text = df.text.apply(process)
+    df.summary = df.summary.apply(process)
+    # return the dataframe
+    return df
+
+
+def summarizer(text, num_sentences=3, sum_custom=LsaSummarizer):
+    # tokenizer
+    tokenizer = Tokenizer(language="english")
+    # parser
+    parser = PlaintextParser.from_string(text, tokenizer)
+    # summarizer
+    summarizerInstance = sum_custom()
+    summary = summarizerInstance(parser.document, num_sentences)
+
+    # join sentences to form a summary
+    summary = " ".join([str(sent) for sent in summary])
+    return summary
+
+
+def calc_rouge(df, col1, col2):
+    rouge = Rouge()
+    # calculate rogue scores
+    r_scores = rouge.get_scores(df[col1], df[col2])
+    df_result = pd.DataFrame(r_scores)
+    return df_result
+
+
+def avg_rouge(df, col1, col2, col3):
+    avg_r1 = 0
+    avg_r2 = 0
+    avg_rL = 0
+    n = df.shape[0]
+    for i in range(n):
+        avg_r1 += df[col1].iloc[i]["r"]
+        avg_r2 += df[col2].iloc[i]["r"]
+        avg_rL += df[col3].iloc[i]["r"]
+
+    avg_r1 = round(avg_r1 / n, 2)
+    avg_r2 = round(avg_r2 / n, 2)
+    avg_rL = round(avg_rL / n, 2)
+
+    # return the average rouge scores
+    return avg_r1, avg_r2, avg_rL
+
+
+def isSentenceInParagraph(sentence, paragraph):
+    list_of_sentences = paragraph.split(".")
+    if sentence in paragraph:
+        return 1
+    return 0
+
+
+def generate_ensemble_summary(row):
+    text_score_dict = {}
+    list_of_sentences_text = row["text"].split(".")
+    total_rogue_l_score = (
+        row["rouge-l_lsa"]["r"]
+        + row["rouge-l_lexrank"]["r"]
+        + row["rouge-l_luhn"]["r"]
+        + row["rouge-l_sum_basic"]["r"]
+        + row["rouge-l_text_rank"]["r"]
+    )
+
+    lsa_weight = row["rouge-l_lsa"]["r"] / total_rogue_l_score
+    lexrank_weight = row["rouge-l_lexrank"]["r"] / total_rogue_l_score
+    luhn_weight = row["rouge-l_luhn"]["r"] / total_rogue_l_score
+    sum_basic_weight = row["rouge-l_sum_basic"]["r"] / total_rogue_l_score
+    text_rank_weight = row["rouge-l_text_rank"]["r"] / total_rogue_l_score
+
+    for sentence in list_of_sentences_text:
+        if sentence == " " or sentence == ' "':
+            continue
+
+        text_score_dict[sentence] = (
+            (isSentenceInParagraph(sentence, row["lsa_summary"]) * lsa_weight)
+            + (isSentenceInParagraph(sentence, row["lexrank_summary"]) * lexrank_weight)
+            + (isSentenceInParagraph(sentence, row["luhn_summary"]) * luhn_weight)
+            + (
+                isSentenceInParagraph(sentence, row["sum_basic_summary"])
+                * sum_basic_weight
+            )
+            + (
+                isSentenceInParagraph(sentence, row["text_rank_summary"])
+                * text_rank_weight
+            )
+        )
+
+    # sort text_score_dict in descending order of scores
+    sorted_text_score_dict = dict(
+        sorted(text_score_dict.items(), key=lambda item: item[1], reverse=True)
+    )
+
+    return ".".join(list(sorted_text_score_dict.keys())[:3])
